fb68616f72f61705983d9989bd41fa444e1d5661
Daniel Chiu <daniechiu@gmail.com>
2015-01-21 21:41:52 -0800

    Update AndroidManifest.xml so there is no application XML.

--- OpenCV4Android/build/intermediates/ndk/debug/obj/local/armeabi/objs/OpenCVLibrary-2.4.9/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o.d
+++
@@ -1,2 +0,0 @@
-/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/build/intermediates/ndk/debug/obj/local/armeabi/objs/OpenCVLibrary-2.4.9//Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o: \
- /Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.c
--- OpenCV4Android/build/intermediates/ndk/debug/obj/local/x86_64/objs/OpenCVLibrary-2.4.9/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o.d
+++
@@ -1,2 +0,0 @@
-/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/build/intermediates/ndk/debug/obj/local/x86_64/objs/OpenCVLibrary-2.4.9//Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o: \
- /Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.c
