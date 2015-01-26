fe267df64d49790570654d10977186cab0c1ac3d
Daniel Chiu <daniechiu@gmail.com>
2015-01-25 19:39:11 -0800

    Removes OpenCV4Android/build directory from source control.

--- OpenCV4Android/build/intermediates/ndk/release/obj/local/x86/objs/OpenCVLibrary-2.4.9/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o.d
+++
@@ -1,2 +0,0 @@
-/Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/build/intermediates/ndk/release/obj/local/x86/objs/OpenCVLibrary-2.4.9//Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.o: \
- /Users/dchiu/Developer/riedel-kruse_lab/BioticGamesAndroidSDK/OpenCV4Android/src/main/jni/empty.c
