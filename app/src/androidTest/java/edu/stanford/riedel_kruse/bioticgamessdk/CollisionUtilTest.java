c3bd49b5fbc50163c3a399ff21ab35608a3ece9d
Daniel Chiu <daniechiu@gmail.com>
2015-01-28 00:19:34 -0800

    Adds additional tests to test collision of rectangles against rectangles.

--- app/src/androidTest/java/edu/stanford/riedel_kruse/bioticgamessdk/CollisionUtilTest.java
+++ app/src/androidTest/java/edu/stanford/riedel_kruse/bioticgamessdk/CollisionUtilTest.java
@@ -9,8 +9,24 @@ import org.opencv.core.Point;
  */
 public class CollisionUtilTest extends TestCase {
     public void testRectangleCollision() {
+        // Case where the two rectangles overlap.
         Rectangle rect1 = new Rectangle(new Point(0, 0), 10, 15, null, -1, true);
-        Rectangle rect2 = new Rectangle(new Point(9, 14), 5, 8, null, -1, true);
+        Rectangle rect2 = new Rectangle(new Point(8, 13), 5, 8, null, -1, true);
+
+        assertEquals(true, CollisionUtil.collided(rect1, rect2));
+
+        // Case where the two rectangles do not overlap.
+        rect2.setPosition(new Point(15, 20));
+
+        assertEquals(false, CollisionUtil.collided(rect1, rect2));
+
+        // Case where the two rectangles touch, but do not overlap.
+        rect2.setPosition(new Point(9, 14));
+
+        assertEquals(true, CollisionUtil.collided(rect1, rect2));
+
+        // Case where one rectangle contains the other.
+        rect2.setPosition(new Point(1, 1));
 
         assertEquals(true, CollisionUtil.collided(rect1, rect2));
     }
