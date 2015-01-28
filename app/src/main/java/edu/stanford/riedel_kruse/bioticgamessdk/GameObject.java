ea30f6fc055bd0e004320f257456c909d414b109
Daniel Chiu <daniechiu@gmail.com>
2015-01-27 17:21:04 -0800

    Updates documentation in GameObject.java.

--- app/src/main/java/edu/stanford/riedel_kruse/bioticgamessdk/GameObject.java
+++ app/src/main/java/edu/stanford/riedel_kruse/bioticgamessdk/GameObject.java
@@ -4,24 +4,25 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 
 /**
- * The DisplayObject class is the parent class for all drawable objects.
+ * The GameObject class is the parent class for all game objects. A GameObject describes how an
+ * entity in a game should be drawn and how it should be modeled physically.
  */
 public abstract class GameObject {
     /**
-     * Instance variable which keeps track of the position of the DisplayObject on the screen.
+     * Instance variable which keeps track of the position of the GameObject on the screen.
      */
     protected Point mPosition;
     /**
-     * Instance variable flag which indicates whether or not this DisplayObject should interact
-     * physically with other DisplayObjects (e.g. collisions).
+     * Instance variable flag which indicates whether or not this GameObject should interact
+     * physically with other GameObjects (e.g. collisions).
      */
     protected boolean mIsPhysical;
 
     /**
-     * Constructor for the DisplayObject class.
-     * @param position the position of the DisplayObject.
-     * @param isPhysical whether or not this DisplayObject should interact physically with other
-     *                   DisplayObjects.
+     * Constructor for the GameObject class.
+     * @param position the position of the GameObject.
+     * @param isPhysical whether or not this GameObject should interact physically with other
+     *                   GameObjects.
      */
     public GameObject(Point position, boolean isPhysical) {
         mPosition = position;
@@ -29,33 +30,34 @@ public abstract class GameObject {
     }
 
     /**
-     * Sets a new position for this DisplayObject.
-     * @param newPosition the new position of the DisplayObject.
+     * Sets a new position for this GameObject.
+     * @param newPosition the new position of the GameObject.
      */
     public void setPosition(Point newPosition) {
         mPosition = newPosition;
     }
 
     /**
-     * Whether or not this DisplayObject should interact physically with other DisplayObjects.
-     * @return
+     * Whether or not this GameObject should interact physically with other GameObjects.
+     * @return true if this GameObject should interact physically with other GameObjects, false
+     *         otherwise.
      */
     public boolean isPhysical() {
         return mIsPhysical;
     }
 
     /**
-     * Draws this DisplayObject to the given frame. Since no origin point is specified, this draws
+     * Draws this GameObject to the given frame. Since no origin point is specified, this draws
      * without translating the object at all.
-     * @param frame the frame to draw the DisplayObject on.
+     * @param frame the frame to draw the GameObject on.
      */
     public void draw(Mat frame) {
         draw(frame, new Point(0, 0));
     }
 
     /**
-     * Draws this DisplayObject to the given frame.
-     * @param frame the frame to draw the DisplayObject on.
+     * Draws this GameObject to the given frame.
+     * @param frame the frame to draw the GameObject on.
      * @param offset the offset to draw at (translates all subsequent draw calls)
      */
     public abstract void draw(Mat frame, Point offset);
