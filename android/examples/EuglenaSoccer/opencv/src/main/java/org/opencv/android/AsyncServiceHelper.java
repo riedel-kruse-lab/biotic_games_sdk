529103ab4ae4358f53f349a9b2329a9d325ffd39
honestykim <honestykim@gmail.com>
2015-09-04 16:21:21 -0700

    Fixes OpenCV crashing issue

--- android/examples/EuglenaSoccer/opencv/src/main/java/org/opencv/android/AsyncServiceHelper.java
+++ android/examples/EuglenaSoccer/opencv/src/main/java/org/opencv/android/AsyncServiceHelper.java
@@ -17,22 +17,21 @@ import android.util.Log;
 
 class AsyncServiceHelper
 {
-    public static boolean initOpenCV(String Version, final Context AppContext,
-            final LoaderCallbackInterface Callback)
-    {
-        AsyncServiceHelper helper = new AsyncServiceHelper(Version, AppContext, Callback);
-        if (AppContext.bindService(new Intent("org.opencv.engine.BIND"),
-                helper.mServiceConnection, Context.BIND_AUTO_CREATE))
-        {
-            return true;
-        }
-        else
-        {
-            AppContext.unbindService(helper.mServiceConnection);
-            InstallService(AppContext, Callback);
-            return false;
-        }
-    }
+     public static boolean initOpenCV(String Version, final Context AppContext,
+        final LoaderCallbackInterface Callback) {
+         AsyncServiceHelper helper = new AsyncServiceHelper(Version, AppContext,
+                 Callback);
+         Intent intent = new Intent("org.opencv.engine.BIND");
+         intent.setPackage("org.opencv.engine");
+         if (AppContext.bindService(intent, helper.mServiceConnection,
+                 Context.BIND_AUTO_CREATE)) {
+             return true;
+         } else {
+             AppContext.unbindService(helper.mServiceConnection);
+             InstallService(AppContext, Callback);
+             return false;
+         }
+     }
 
     protected AsyncServiceHelper(String Version, Context AppContext, LoaderCallbackInterface Callback)
     {
